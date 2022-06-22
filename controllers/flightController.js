a8bfb3b003b5b73917c0a93d1c316628df10b957
Onyedikachi <onyedikachinwosu@rocketmail.com>
2022-06-22 07:55:51 +0100

    add endpiont to get one flight

--- controllers/flightController.js
+++ controllers/flightController.js
@@ -6,4 +6,10 @@ exports.listAll = (req, res) => {
     return res.status(200).json(flights);
 }
 
+exports.listOne = (req, res) => {
+    const { id } =  req.params;
+
+    return res.status(200).json(flights.filter((id)=> flights.id === id));
+}
+
 
--- routes/flightRoute.js
+++ routes/flightRoute.js
@@ -3,7 +3,8 @@ const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/flightController');
 
-router.get('/flights', controller.listAll)
+router.get('/flights', controller.listAll);
+router.get('/flights/:id', controller.listOne);
 
 module.exports = router;
 
